56c8e1cf941d01469a6295f40888d30ebe96054a
yohei-ito <itoyo@jp.ibm.com>
2021-05-10 08:11:17 +0000

    contact add

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # path for about view
     path('about/', views.about, name='about'),
     # path for contact us view
-
+    path('contact/', views.contact, name='contact'),
     # path for registration
 
     # path for login
